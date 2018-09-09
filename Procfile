c5ac84b3710a7b7c9b21a32e67b29b2eb3f65b3f
Yana <yana.lysenkova@gmail.com>
2018-09-10 00:14:59 +0300

    Configuration is added

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: java -jar target/soap-shop-1.0-SNAPSHOT.jar
+web: java $JAVA_OPTS -Dspring.profiles.active='heroku' -jar target/soap-shop-1.0-SNAPSHOT.jar
+release: ./mvnw liquibase:update
--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,25 @@
-CREATE SCHEMA IF NOT EXISTS soapstore
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
+CREATE SCHEMA soapshop;
 
-CREATE TABLE users (
-  id         BIGINT AUTO_INCREMENT,
-  login      VARCHAR(100),
-  password   VARCHAR(100),
-  salt       VARCHAR(100),
-
-  PRIMARY KEY (id)
+CREATE TABLE soapshop.users
+(
+  id  SERIAL PRIMARY KEY,
+  password character varying(100),
+  salt character varying(100),
+  login character varying(100)
+)
+WITH (
+OIDS=FALSE
 );
 
-CREATE TABLE products (
-  id    BIGINT AUTO_INCREMENT,
-  name  VARCHAR(100),
-  price DOUBLE,
-  image_ref VARCHAR (500),
-  date  DATETIME DEFAULT CURRENT_TIMESTAMP ,
-
-  PRIMARY KEY (id)
+CREATE TABLE soapshop.products
+(
+  id SERIAL PRIMARY KEY,
+  name character varying(100),
+  price double precision,
+  image_ref character varying(500),
+  date timestamp without time zone
+)
+WITH (
+OIDS=FALSE
 );
+
